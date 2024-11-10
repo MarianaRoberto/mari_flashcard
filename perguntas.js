2b6b0202ca07794d9afb6a056edd69e539e7237b
MarianaRoberto <00001107635792sp@al.educacao.sp.gov.br>
2024-11-10 08:13:30 -0300

    cartões

---
+++ perguntas.js
@@ -0,0 +1,53 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em Inglês é HI (RAI)'
+)
+
+criaCartao(
+    'Invenções',
+    'De onde é a invenção do chuveiro elétrico?',
+    'O primeiro chuveiro elétrico foi desenvolvido no Brasil, na década de 40, pelo Francisco Canhos'
+)
+
+criaCartao(
+    'Química',
+    'Atualmente, quantos elementos químicos a tabela periódica possui?',
+    'Até o momento, ele possui 118 elementos (Última atualização: 2016)'
+)
+
+criaCartao(
+    'Datas comemorativas',
+    'Quais são as duas principais datas comemoradas em novembro?',
+    'No dia 15/11 comemora-se a Proclamação da República e no dia 20/11, o Dia Nacional da Consciência Negra'
+)
+
+criaCartao(
+    'Folclore',
+    'Que personagem folclórico costuma ser agradado pelos caçadores com a oferta de fumo?',
+    'A Caipora. Ela costuma assusta os caçadores, então o fumo de corda é deixado junto a um tronco de árvore com o intuito de agradar a Caipora para, assim, os caçadores poderem caçar'
+)
+
+criaCartao(
+    'História',
+    'Em que período da pré-história o fogo foi descoberto?',
+    'Foi no Paleolítico que o fogo começou a ser utilizado, quando os homens aprenderam que era possível obter fogo por meio do atrito de pedaços de madeira e pedra'
+)
